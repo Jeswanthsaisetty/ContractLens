1736db4096c5999944f0ff522d59cf0fdd9ff756
Jeswanthsaisetty <jeswanthroyal126@gmail.com>
2026-09-20 09:13:15 +0530

    Modify CORS middleware allowed origins
    Updated CORS origins to include a new production URL.

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,11 +25,11 @@ app = FastAPI(
 # Allow the React/Vite frontend to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+   allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://contract-lens-p3bkhh9eb-jeswanthsaisettys-projects.vercel.app/",
-],
+    "https://contract-lens-pqgkzvb6h-jeswanthsaisettys-projects.vercel.app",
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
