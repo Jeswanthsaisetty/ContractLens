df3ebd05dbd6bbfaf0fba1afab47470158c7d16f
Jeswanthsaisetty <jeswanthroyal126@gmail.com>
2026-09-20 10:10:26 +0530

    Update CORS origin for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
    allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://contract-lens-pqgkzvb6h-jeswanthsaisettys-projects.vercel.app",
+    "https://contract-lens-d66y616z5-jeswanthsaisettys-projects.vercel.app/",
 ],
     allow_credentials=True,
     allow_methods=["*"],
