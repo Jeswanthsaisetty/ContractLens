be1aec0ea05dae93324a38721e50075c63343dc7
Jeswanthsaisetty <jeswanthroyal126@gmail.com>
2026-09-20 09:53:51 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://contract-lens-pqgkzvb6h-jeswanthsaisettys-projects.vercel.app",
-]
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
